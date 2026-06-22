a20a1151f12d145e79c238519216768f14fb3f50
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2026-06-23 00:27:48 +0530

    delete

--- server/models/Post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PostSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  body: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Post', PostSchema);
